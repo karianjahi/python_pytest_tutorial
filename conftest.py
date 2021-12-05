062cbe8f37d711e8513885c1b7691d4f36413906
Joseph Njeri <njeri@edi.gmbh>
2021-12-05 19:45:25 +0100

    lint all .py files

--- conftest.py
+++ conftest.py
@@ -3,21 +3,32 @@ Creating a python module for test data and add a fixture decorator
 """
 import pytest
 
-text_file = "data/some_corpus.txt"
+TEXT_FILE = "data/some_corpus.txt"
 
 @pytest.fixture
 def text_summary():
-    with open(text_file, "r") as f:
-        return f.read()
+    """
+    Reading the file
+    """
+    with open(TEXT_FILE, "r") as file:
+        return file.read()
 
-"""
-- A class example
-class MyFixture:
-    def read_file(file):
-        with open(file, "r") as f:
-            return f.read()
 
-@pytest.fixture
-def text_summary():
-    return MyFixture.read_file(text_file)
-"""
+# # - A class example
+# class MyFixture:
+#     """
+#     Running a fixture in class
+#     """
+#     def read_file(self, file):
+#         """
+#         read file
+#         """
+#         with open(file, "r") as afile:
+#             return afile.read()
+
+# @pytest.fixture
+# def text_summary():
+#     """
+#     Implement class
+#     """
+#     return MyFixture.read_file(TEXT_FILE)
